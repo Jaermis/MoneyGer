6854dcfe71ac12c4f25d6fec36b35e519445ff65
Jaermis <jermynejosh.kaquilala@cit.edu>
2024-02-27 16:16:36 +0800

    comitt1

---
+++ moneyger.client/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
