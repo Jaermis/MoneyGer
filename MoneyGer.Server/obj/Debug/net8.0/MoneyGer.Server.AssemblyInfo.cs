f45c9d55eee4245a1943b4b31bc4d9e89b88ff54
Jaermis <jermynejosh.kaquilala@cit.edu>
2024-02-28 18:26:27 +0800

    Added launch.json

--- MoneyGer.Server/obj/Debug/net8.0/MoneyGer.Server.AssemblyInfo.cs
+++ MoneyGer.Server/obj/Debug/net8.0/MoneyGer.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoneyGer.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603db90a33a51f75c2074104bf389cc752b42bd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61de95dc9b3a91526fd66fc52c7d59e76779f94c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoneyGer.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoneyGer.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
