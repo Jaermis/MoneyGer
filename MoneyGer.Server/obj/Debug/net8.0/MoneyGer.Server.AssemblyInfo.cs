2be26e1eb78567ed43072c17aa411e9c6141fbc9
Jaermis <jermynejosh.kaquilala@cit.edu>
2024-03-04 21:58:34 +0800

    Added password visibility and  fixed signup layout

--- MoneyGer.Server/obj/Debug/net8.0/MoneyGer.Server.AssemblyInfo.cs
+++ MoneyGer.Server/obj/Debug/net8.0/MoneyGer.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoneyGer.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a155ddff9286b43a8010a178f287b52786660914")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d2edb836936f45a87b831ed92190a18b312fd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoneyGer.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoneyGer.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- moneyger.client/src/app/login/login.component.ts
+++ moneyger.client/src/app/login/login.component.ts
@@ -12,7 +12,16 @@ export class LoginComponent implements OnInit {
     private titleService: Title,
     ) { }
 
+  changeicon:boolean = true;
+  changetype:boolean = true;
+  showImg: boolean = false;
+
   ngOnInit(): void {
     this.titleService.setTitle('MoneyGer Login');
+  }
+
+  viewpass(){
+    this.changetype = !this.changetype;
+    this.changeicon = !this.changeicon;
   }
 }
