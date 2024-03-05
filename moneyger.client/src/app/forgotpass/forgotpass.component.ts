c89765a73c443c4a12798efd8d4c151ff98b4255
Jaermis <jermynejosh.kaquilala@cit.edu>
2024-03-05 18:45:36 +0800

    Josh[Add][ForgotpassRouting]

--- moneyger.client/src/app/forgotpass/forgotpass.component.ts
+++ moneyger.client/src/app/forgotpass/forgotpass.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgotpass',
   templateUrl: './forgotpass.component.html',
   styleUrl: './forgotpass.component.css'
 })
-export class ForgotpassComponent {
+export class ForgotpassComponent implements OnInit {
+  constructor(
+    private titleService: Title,
+    ) { }
 
+    ngOnInit(): void {
+      this.titleService.setTitle('Forgot Password');
+    }
 }
