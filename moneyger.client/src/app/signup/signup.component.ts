ed33bc8140e00861b25d884ee42fe77c414db3b9
Jaermis <jermynejosh.kaquilala@cit.edu>
2024-03-05 11:51:15 +0800

    Added password visibility to Signup

--- moneyger.client/src/app/signup/signup.component.ts
+++ moneyger.client/src/app/signup/signup.component.ts
@@ -10,7 +10,15 @@ export class SignupComponent implements OnInit {
   constructor(
     private titleService: Title){}
 
+    changeicon:boolean = true;
+    changetype:boolean = true;
+
     ngOnInit(): void {
       this.titleService.setTitle('MoneyGer Signup');
+    }
+
+    viewpass(){
+      this.changeicon = !this.changeicon;
+      this.changetype = !this.changetype;
     }
 }
