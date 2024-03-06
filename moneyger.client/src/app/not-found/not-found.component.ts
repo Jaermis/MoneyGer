207cd287de0700bc36cfac9db99fd1212eff14be
Jaermis <jermynejosh.kaquilala@cit.edu>
2024-03-06 14:47:21 +0800

    Josh[Add][AddedNotFoundRouting]

--- moneyger.client/src/app/not-found/not-found.component.ts
+++ moneyger.client/src/app/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
+  constructor(private router: Router) {}
 
+  toLogin(){
+    this.router.navigate(['/login']);
+  }
 }
