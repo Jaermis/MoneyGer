fff41365ff072a235d63721d893a91aeaf3c7c93
Jaermis <jermynejosh.kaquilala@cit.edu>
2024-03-08 20:30:19 +0800

    Database setup done

--- MoneyGer.Server/obj/Release/net8.0/MoneyGer.Server.AssemblyInfo.cs
+++ MoneyGer.Server/obj/Release/net8.0/MoneyGer.Server.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoneyGer.Server")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c411ac97e6db25f3795fdc8aef70c6d0f51a956")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoneyGer.Server")]
