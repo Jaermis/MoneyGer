360059201b513d81f92b9aaea0fac97dd7e0cc1e
unknown <takashimaretubado@gmail.com>
2024-03-16 02:23:43 +0800

    [JAMES][UPDATED]FUNCTIONAL LOGIN SERVERSIDE

---
+++ MoneyGer.Server/Models/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace MoneyGer.Server.Models
+{
+    public class Login
+    {
+        public required string WorkEmail { get; set; }
+        public required string UserPassword { get; set; }
+    }
+}
--- MoneyGer.Server/obj/Debug/net8.0/MoneyGer.Server.AssemblyInfo.cs
+++ MoneyGer.Server/obj/Debug/net8.0/MoneyGer.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoneyGer.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a3c12703ee3066c3356384f9e09ee8535350d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a865ac1e8b1f4b2b34de9d86b2c318576bb85981")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoneyGer.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoneyGer.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
