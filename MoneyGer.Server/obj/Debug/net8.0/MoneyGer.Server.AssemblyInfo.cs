2eaade9ad3785efe565ec5c0117c91cae0d72912
Jule Jerrish Quijano <julr789@gmail.com>
2024-05-07 20:59:05 +0800

    not finished calendar

--- MoneyGer.Server/obj/Debug/net8.0/MoneyGer.Server.AssemblyInfo.cs
+++ MoneyGer.Server/obj/Debug/net8.0/MoneyGer.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoneyGer.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed635234a352498bc1e2cc40c21ea6d4bb15cd13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48983220fe425e08f26594598c9fb5e66ab8a615")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoneyGer.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoneyGer.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
