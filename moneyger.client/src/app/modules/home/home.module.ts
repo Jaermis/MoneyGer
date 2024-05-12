16e89b3b88e998b22b5fa7b4d95d1405d1e84086
Jaermis <jermynejosh.kaquilala@cit.edu>
2024-05-12 21:52:52 +0800

    [JOSH] [add] Home contents in the landing page pc pa

--- moneyger.client/src/app/modules/home/home.module.ts
+++ moneyger.client/src/app/modules/home/home.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ScrollSpyModule } from 'ngx-scrollspy';
 
 
 @NgModule({
@@ -15,7 +14,6 @@ import { ScrollSpyModule } from 'ngx-scrollspy';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    //ScrollSpyModule.forRoot()
   ]
 })
 export class HomeModule { }
