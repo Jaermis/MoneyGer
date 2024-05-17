0606e0b0230302f6cf0774935cf308a721692752
James Kenneth <134383514+jimskenet@users.noreply.github.com>
2024-05-18 07:28:00 +0800

    Revert "Jule link message"

--- MoneyGer.Server/EmailService/EmailSender.cs
+++ MoneyGer.Server/EmailService/EmailSender.cs
@@ -12,60 +12,22 @@ public class EmailSender : IEmailSender
     }
 
     public async Task SendEmailAsync(string email, string subject, string htmlMessage)
-{
-    var mimeMessage = new MimeMessage();
-    mimeMessage.From.Add(new MailboxAddress(_emailConfiguration.FromName, _emailConfiguration.FromAddress));
-    mimeMessage.To.Add(new MailboxAddress("", email));
-    mimeMessage.Subject = subject;
-
-    var bodyBuilder = new BodyBuilder();
-
-    // Combine the HTML template with the actual htmlMessage content
-    bodyBuilder.HtmlBody = "<html>" +
-                           "<head>" +
-                           "<style>" +
-                           "body {" +
-                           "    font-family: Arial, sans-serif;" +
-                           "    background-color: #f5f5f5;" +
-                           "    color: #333;" +
-                           "}" +
-                           ".container {" +
-                           "    width: 80%;" +
-                           "    margin: 0 auto;" +
-                           "    padding: 20px;" +
-                           "    background-color: #fff;" +
-                           "    border-radius: 5px;" +
-                           "    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);" +
-                           "}" +
-                           "h1 {" +
-                           "    color: #007bff;" +
-                           "}" +
-                           "p {" +
-                           "    line-height: 1.6;" +
-                           "}" +
-                           "</style>" +
-                           "</head>" +
-                           "<body>" +
-                           "<div class='container'>" +
-                           "<h1>MoneyGer Email Confirmation</h1>" +
-                           "<p>Please click the link below to confirm your email address:</p>" +
-                           htmlMessage +
-                           "<p>If you didn't request this, you can safely ignore this email.</p>" +
-                           "<p>Best regards,</p>" +
-                           "<p>MoneyGer</p>" +
-                           "</div>" +
-                           "</body>" +
-                           "</html>";
-
-    mimeMessage.Body = bodyBuilder.ToMessageBody();
-
-    using (var client = new SmtpClient())
     {
-        await client.ConnectAsync(_emailConfiguration.SmtpServer, _emailConfiguration.Port, _emailConfiguration.UseSsl);
-        await client.AuthenticateAsync(_emailConfiguration.Username, _emailConfiguration.Password);
-        await client.SendAsync(mimeMessage);
-        await client.DisconnectAsync(true);
-    }
-}
+        var mimeMessage = new MimeMessage();
+        mimeMessage.From.Add(new MailboxAddress(_emailConfiguration.FromName, _emailConfiguration.FromAddress));
+        mimeMessage.To.Add(new MailboxAddress("",email));
+        mimeMessage.Subject = subject;
 
+        var bodyBuilder = new BodyBuilder();
+        bodyBuilder.HtmlBody = htmlMessage;
+        mimeMessage.Body = bodyBuilder.ToMessageBody();
+        
+        using (var client = new SmtpClient())
+        {
+            await client.ConnectAsync(_emailConfiguration.SmtpServer, _emailConfiguration.Port, _emailConfiguration.UseSsl);
+            await client.AuthenticateAsync(_emailConfiguration.Username, _emailConfiguration.Password);
+            await client.SendAsync(mimeMessage);
+            await client.DisconnectAsync(true);
+        }
+    }
 }
