edbdc336746da77ea0f83d57a5d9847ee372ddcf
Ronerr Villacarlos <rvillacarlos68@gmail.com>
2024-05-18 01:17:45 +0800

    Revert "Merge pull request #59 from Jaermis/profile"
    This reverts commit 5ff207fd21de46b0f5eec9260613683164af6e87, reversing
    changes made to df2be1a5ba1ce80cc214dbf575b44bed10e4612d.

--- moneyger.client/src/app/modules/user/user-routing.module.ts
+++ moneyger.client/src/app/modules/user/user-routing.module.ts
@@ -10,8 +10,6 @@ import { OrdersComponent } from './components/orders/orders.component';
 import { StatisticsComponent } from './components/statistics/statistics.component';
 import { CreateCompanyComponent } from './components/create-company/create-company.component';
 import { JoinCompanyComponent } from './components/join-company/join-company.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
 
 const routes: Routes = [
   {
@@ -26,9 +24,7 @@ const routes: Routes = [
       {path: 'statistics', component: StatisticsComponent},
       {path: '', component: HomeComponent},
       {path: 'create-company', component: CreateCompanyComponent},
-      {path: 'join-company', component: JoinCompanyComponent},
-      {path: 'profile', component: ProfileComponent},
-      {path: 'edit-profile', component: EditProfileComponent}
+      {path: 'join-company', component: JoinCompanyComponent}
   ]}
 ];
 
