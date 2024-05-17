6344d41a5238c170983c381d2bc2e8f3bbbe0725
Ronerr Villacarlos <rvillacarlos68@gmail.com>
2024-05-17 23:02:33 +0800

    [RONERR][MIGRATIONS][EVENTSANDATTENDEES]

---
+++ MoneyGer.Server/Dtos/AssignAttendeeDto.cs
@@ -0,0 +1,10 @@
+
+
+namespace MoneyGer.Server.Dtos
+{
+    public class AssignAttendeeDto
+    {
+        public string? UserId { get; set; }
+        public int DateId {get; set; }
+    }
+} 
---
+++ MoneyGer.Server/Dtos/Events.cs
@@ -0,0 +1,11 @@
+namespace MoneyGer.Server.Dtos
+{
+    public class Events
+    {
+        public string? Description { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public DateTime Time{get; set; }
+    }
+
+}
